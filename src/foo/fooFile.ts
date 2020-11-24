df22ca4144e95e7073006f104ae2eeaae06395a0
David Johnston <djohnston@whispir.com>
2020-11-24 15:34:43 +1100

    initial commit

---
+++ src/foo/fooFile.ts
@@ -0,0 +1,20 @@
+
+import { fileName } from "../bar";
+
+
+
+export const test = "test"; 
+
+
+const myData = {
+    data: fileName
+}; 
+
+
+export function main() {
+    console.log(myData); 
+    console.log(fileName); 
+}
+
+
+
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import {main} from "./foo/fooFile";
+
+export * from "./bar"; 
+export * from "./foo/fooFile"; 
+
+main();
